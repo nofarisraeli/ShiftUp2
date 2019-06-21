217e524d898a1ae1f77c59d1eec4f9e1420b1cd2
yonathantzur1 <yonathantzur1@gmail.com>
2019-06-21 20:51:04 +0300

    Add socket io package

---
+++ modules/socket.js
@@ -0,0 +1,14 @@
+module.exports = (io) => {
+    io.on('connection', (socket) => {
+
+        socket.on('login', () => {
+
+        });
+
+        socket.on('disconnect', () => {
+
+        });
+
+    });
+}
+
