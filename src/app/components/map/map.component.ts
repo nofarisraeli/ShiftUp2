a3e97cd5e01c18e23ce2529361a0cf0f946d87f3
nivhojberg <nivhojberg@hotmail.com>
2019-08-02 00:14:23 +0300

    map bug fixed

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-// import Microsoft from ''
+import { Component, OnInit } from '@angular/core';
 
+declare let Microsoft: any;
 // declare let $: any;
 
 @Component({
@@ -11,30 +11,28 @@ import { Component } from '@angular/core';
 })
 
 export class MapComponent {
-    
-    bingMapCallback = () => {
-        console.log("bingMapCallback");
-        // let map: any = new Microsoft.Maps.Map(document.getElementById("bingMap"), {
-        //     credentials: 'uPvgq4Nh7bSJpV8LKmmB~L_aavPOS7_b9Tp4Bo2X8WQ~As2x0q-bbYoeLGwybYQpvBLT-OkwrPOQu9W7oJ8hcnQEpGnKawwTQzEEzPPpfxbu',
-        //     center: new Microsoft.Maps.Location(0, 0),
-        //     zoom: 1,
-        // });
+
+    /// <reference path="types/MicrosoftMaps/CustomMapStyles.d.ts" />
+    /// <reference path="types/MicrosoftMaps/Microsoft.Maps.d.ts" />
+    /// <reference path="types/MicrosoftMaps/Microsoft.Maps.All.d.ts" />
+
+
+    ngOnInit() {
+        var map = new Microsoft.Maps.Map("#workersBingMap", {
+           credentials: 'uPvgq4Nh7bSJpV8LKmmB~L_aavPOS7_b9Tp4Bo2X8WQ~As2x0q-bbYoeLGwybYQpvBLT-OkwrPOQu9W7oJ8hcnQEpGnKawwTQzEEzPPpfxbu',
+            center: new Microsoft.Maps.Location(31, 35),
+            zoom: 7,
+        });
+            
+        let infobox: any = new Microsoft.Maps.Infobox(map.getCenter(), { visible: true });
+        infobox.setMap(map);
+
+        var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(50, 10), { title: "test 1" });
         
-        // Add an infobox to the map so that we can display it when a pushpin is clicked.
-        // let infobox: any = new Microsoft.Maps.Infobox(map.getCenter(), { visible: false });
-        // infobox.setMap(map);
-        
-        // // Create the point pushpin of the current point
-        // let pushpin: any = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(0, 0), { title: "test" });
-        
-        // // Define the click event of the point
-        // Microsoft.Maps.Events.addHandler(pushpin, 'click', function (e) {
-        //     // Get the point name by the clicked coordinates
-        //     infobox.setOptions({
-        //         location: e.primitive.getLocation(),
-        //         description: "test infobox",
-        //         visible: true
-        //     })
-        // });
+        // Define the click event of the point
+        Microsoft.Maps.Events.addHandler(pushpin, 'click', function (e: any) {
+            console.log("pushpin click 1");
+            infobox.setOptions({ location: e.primitive.getLocation(), description: "test infobox", visible: true });
+        });
     }
 }
